f80091556ad24ce18df0a22e53257116f32645e9
Newton <nkafle49@gmail.com>
2023-02-01 23:22:35 +1030

    boxes are working, ui updated

--- playground.py
+++ playground.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+
+from Tic_Tac_Toe.event_handler import click
+
+
 class Playground(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -16,6 +20,7 @@ class Playground(tk.Tk):
 
         # player 1 label and score
         self.player1 = ttk.Label(self, text="Player 1", font=("Arial", 20, 'bold'))
+        self.player1
         self.player1.grid(column=0, row=4)
         self.player1_score = ttk.Label(self, text="Score:", font=("Arial", 15))
         self.player1_score.grid(column=0, row=5, sticky='w')
@@ -33,12 +38,12 @@ class Playground(tk.Tk):
 
         #create playground
         self.name = ['A', 'B', 'C']
-        self.names_dic = {}
+        self.button_list = []
         for row in range(3):
             for col in range(3):
-                self.names_dic[f"{self.name[row]}{col}"] = ttk.Label(self.ground_frame, text='__', borderwidth=1, relief='solid',
-                             font=('Arial', 80))
-                self.names_dic[f"{self.name[row]}{col}"].grid(column=col, row=row)
+                self.button_list.append(tk.Button(self.ground_frame, text="", height=5, width=5,
+                                   command= lambda m=f'{self.name[row]}{col}': click(m))
+                                        .grid(column=col, row=row))
 
 
 
